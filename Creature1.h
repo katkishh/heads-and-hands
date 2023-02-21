e4cdcea4940d84b1df792a1d17d2f0fce90a6512
katkishh <105926462+katkishh@users.noreply.github.com>
2023-02-21 17:07:53 +0700

    Add files via upload

---
+++ Creature1.h
@@ -0,0 +1,140 @@
+﻿#pragma once
+#include<iostream>
+#include <stdlib.h>
+#include<string>
+#include<cmath>
+#include <time.h>
+
+class Creature {
+public:
+
+	Creature(int attack, int defence, int health,int minDamage, int maxDamage) {
+		m_attack = attack;
+		if ( attack < 1 ) {
+			std::cerr << "Неверное значение минимальной атаки. Минимальная атака по умолчанию: 1" << std::endl;
+			m_attack = 1;
+		}
+		if ( attack > 20 ) {
+			std::cerr << "Неверное значение максимальной атаки. Максимальная атака по умолчанию: 20" << std::endl;
+			m_attack = 20;
+		}
+
+		m_defence = defence;
+		if ( defence < 1 ) {
+			std::cerr << "Неверное значение защиты, введено слишком малое значение. Защита по умолчанию: 1" << std::endl;
+			m_defence = 1;
+		}
+		if ( defence > 20 ) {
+			std::cerr << "Неверное значение защиты, введено слишком большое значение. Защита по умолчанию: 20" << std::endl;
+			m_defence = 20;
+		}
+
+		m_maxHealth = health;
+		if ( health < 1 ) { 
+			std::cerr << "Неверное значение здоровья. Здоровье по умолчанию: 1" << std::endl;
+			m_maxHealth = 1;
+		}
+		m_currentHealth = m_maxHealth;
+		
+		m_minDamage = minDamage;
+		m_maxDamage = maxDamage;
+		if ( minDamage < 1 ) {
+			std::cerr << "Неверное значение минимального урона, введено слишком малое значение. Минимальный урон по умолчанию: 1" << std::endl;
+			m_minDamage = 1;
+			minDamage = 1;
+		}
+		if ( maxDamage < 1 ) {
+			std::cerr << "Неверное значение максимального урона, введено слишком малое значение. Максимальный урон по умолчанию: 1" << std::endl;
+			m_maxDamage = 1;
+			maxDamage = 1;
+		}
+		if ( minDamage > maxDamage ) {
+			std::cerr << "Ошибка ввода минимального и максимального уронов. Значения поменялись местами" << std::endl;
+			m_maxDamage = minDamage;
+			m_minDamage = maxDamage;
+		}
+	};
+	~Creature() {};
+
+	bool isAlive() {
+		return ( m_currentHealth > 0 ) ? true : false;
+	}
+
+	void Attack( Creature& victim ) {
+		int modAtk = m_attack - victim.m_defence + 1;
+		bool flag = false;
+		srand(time(NULL));
+		int number = rand() % 6 + 1;
+		flag = ( number == 5 || number == 6 ) ? true : false;
+		if ( !flag ) {
+			for ( int i = 1; i < modAtk; ++i ) {
+				number = rand() % 6 + 1;
+				flag = ( number == 5 || number == 6 ) ? true : false;
+				if ( flag ) break;				//зачем генерировать все остальные, если наткнулись на 5 или 6?
+			}
+		}
+		if (flag) {
+			int damage = rand() % (m_maxDamage - m_minDamage + 1) + m_minDamage + 1;
+			std::cout << "Нанесено урона: " << damage << std::endl;
+			victim.m_currentHealth -= damage;
+		}
+		else std::cout << "Попытка нанесения урона не увенчалась успехом" << std::endl;
+	}
+
+	int getAttack() {
+		return m_attack;
+	}
+
+	int getDefence() {
+		return m_defence;
+	}
+
+	int getMaxHealth() {
+		return m_maxHealth;
+	}
+
+	int getCurrentHealth() {
+		return m_currentHealth;
+	}
+
+	int getMinDMG() {
+		return m_minDamage;
+	}
+
+	int getMaxDMG() {
+		return m_maxDamage;
+	}
+
+protected:
+	int m_attack;
+	int m_defence;
+	int m_maxHealth;
+	int m_currentHealth;
+	int m_minDamage;
+	int m_maxDamage;
+};
+
+class Player : public Creature {
+	
+public:
+	Player(int attack, int defence, int health, int minDamage, int maxDamage) : Creature(attack, defence, health, minDamage, maxDamage) {};
+	~Player() {};
+
+	void Cure() {
+		if ( m_cureCounter < 3 ) {
+			m_cureCounter += 1;
+			m_currentHealth += round(0.5 * m_maxHealth);
+			m_currentHealth = (m_currentHealth < m_maxHealth) ? m_currentHealth : m_maxHealth;			//поправка на пере-хилл
+		}
+		else std::cout << "Вы достигли лимита лечения" << std::endl;
+	}
+
+private:
+	uint32_t m_cureCounter{};
+};
+
+class Monster : public Creature {
+public:
+	Monster(int attack, int defence, int health, int minDamage, int maxDamage) : Creature(attack, defence, health, minDamage, maxDamage) {};
+	~Monster() {};
+};
---
+++ head_hands1.cpp
@@ -0,0 +1,185 @@
+﻿#include "Creature.h"
+
+bool isCorrect() {
+	if ( !std::cin )
+	{
+		std::cin.clear();
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		return false;
+	}
+	return true;
+}
+
+Player PlayerCreation() {
+	int attack, defence, health, minDMG, maxDMG;
+	std::cout << "\t" << "сила атаки: ";
+	std::cin >> attack;
+	while ( !isCorrect() ) {
+		std::cout << std::endl << "Неверное значение атаки, повторите еще раз" << std::endl;
+		std::cin >> attack;
+	}
+
+	std::cout << "\t" << "показатель защиты: ";
+	std::cin >> defence;
+	while ( !isCorrect() ) {
+		std::cout << std::endl << "Неверное значение защиты, повторите еще раз" << std::endl;
+		std::cin >> defence;
+	}
+
+	std::cout <<  "\t" << "показатель здоровья: ";
+	std::cin >> health;
+	while ( !isCorrect() ) {
+		std::cout << std::endl << "Неверное значение здоровья, повторите еще раз" << std::endl;
+		std::cin >> health;
+	}
+
+	std::cout <<  "\t" << "минимальный наносимый урон: ";
+	std::cin >> minDMG;
+	while ( !isCorrect() ) {
+		std::cout << std::endl << "Неверное значение минимального урона, повторите еще раз" << std::endl;
+		std::cin >> minDMG;
+	}
+
+	std::cout <<  "\t" << "максимальный наносимый урон: ";
+	std::cin >> maxDMG;
+	while ( !isCorrect() ) {
+		std::cout << std::endl << "Неверное значение максимального урона, повторите еще раз" << std::endl;
+		std::cin >> maxDMG;
+	}
+
+	Player yourPlayer = Player(attack, defence, health, minDMG, maxDMG);
+	return yourPlayer;
+}
+
+Monster MonsterCreation() {
+	std::cout << std::endl << "Желаете подобрать противника? Введите 0, если да: ";
+	int attack, defence, health, minDMG, maxDMG;
+	std::string ans;
+	std::cin >> ans;
+
+	if ( ans == "0" ) {
+		std::cout << "Сила атаки: ";
+		std::cin >> attack;
+		while ( !isCorrect() ) {
+			std::cout << std::endl << "Неверное значение атаки, повторите еще раз" << std::endl;
+			std::cin >> attack;
+		}
+
+		std::cout << "Показатель защиты: ";
+		std::cin >> defence;
+		while ( !isCorrect() ) {
+			std::cout << std::endl << "Неверное значение защиты, повторите еще раз" << std::endl;
+			std::cin >> defence;
+		}
+
+		std::cout << "Показатель здоровья: ";
+		std::cin >> health;
+		while ( !isCorrect() ) {
+			std::cout << std::endl << "Неверное значение здоровья, повторите еще раз" << std::endl;
+			std::cin >> health;
+		}
+
+		std::cout << "Минимальный наносимый урон: ";
+		std::cin >> minDMG;
+		while ( !isCorrect() ) {
+			std::cout << std::endl << "Неверное значение минимального урона, повторите еще раз" << std::endl;
+			std::cin >> minDMG;
+		}
+
+		std::cout << "Максимальный наносимый урон: ";
+		std::cin >> maxDMG;
+		while ( !isCorrect() ) {
+			std::cout << std::endl << "Неверное значение максимального урона, повторите еще раз" << std::endl;
+			std::cin >> maxDMG;
+		}
+	}
+	else {
+		srand(time(NULL));
+		attack = rand() % 20 + 1;
+		defence = rand() % 20 + 1;
+		health = rand() % 50 + 1;
+		minDMG = rand() % 25 + 1;
+		maxDMG = rand() % 25 + 1;
+		int save = minDMG;
+		if ( minDMG > maxDMG ) {
+			minDMG = maxDMG;
+			maxDMG = save;
+		}
+		std::cout << "Сила атаки: " << attack << std::endl;
+		std::cout << "Показатель защиты: " << defence << std::endl;
+		std::cout << "Показатель здоровья: " << health << std::endl;
+		std::cout << "Минимальный наносимый урон: " << minDMG << std::endl;
+		std::cout << "Максимальный наносимый урон: " << maxDMG << std::endl;
+	}
+
+	Monster mon = Monster(attack, defence, health, minDMG, maxDMG);
+
+	return mon;
+}
+
+void PerformMove(Player& player, Monster& mon1, Monster& mon2, Monster& mon3, int& monsterCounter) {
+
+	std::cout << std::endl << "Текущий уровень здоровья равен " << player.getCurrentHealth() << ". Использовать лечащий пузырек? Введите 0, если хотите излечиться: ";
+	std::string ans;
+	std::cin >> ans;
+	if ( ans == "0" ) player.Cure();
+	std::cout << "Вы нападаете на противника" << std::endl;
+	if ( mon1.isAlive() ) { 
+		player.Attack(mon1);
+		if ( !mon1.isAlive() ) { 
+			std::cout << "Первый противник повержен!"  << std::endl;
+			monsterCounter -= 1;
+		}
+		else {
+			std::cout << "Монстр атакует вас!" << std::endl;
+			mon1.Attack(player);
+		}
+	}
+	else {
+		if ( mon2.isAlive() ) {
+			player.Attack(mon2);
+			if ( !mon2.isAlive() ) { 
+				std::cout << "Второй противник повержен!" << std::endl;
+				monsterCounter -= 1;
+			}
+			else {
+				std::cout << "Монстр атакует вас!" << std::endl;
+				mon2.Attack(player);
+			}
+		}
+		else {
+			player.Attack(mon3);
+			if ( !mon3.isAlive() ) { 
+				std::cout << "Третий противник повержен!" << std::endl;
+				monsterCounter -= 1;
+			}
+			else {
+				std::cout << "Монстр атакует вас!" << std::endl;
+				mon3.Attack(player);
+			}
+		}
+	}
+
+}
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	std::cout << "Добро пожаловать в игру P&M!" << std::endl << "Пожалуйста, создайте своего игрока, для этого вам нужно ввести следующие показатели:" << std::endl;
+	Player yourPlayer = PlayerCreation();
+
+	std::cout << std::endl << "Отлично! Вы выступаете против трёх монстров." << std::endl;
+	int monsterCounter = 3;
+	Monster mon1 = MonsterCreation();
+	Monster mon2 = MonsterCreation();
+	Monster mon3 = MonsterCreation();
+
+	std::cout << "Да начнется игра. И пусть удача всегда будет на вашей стороне!" << std::endl;
+	do {
+		PerformMove(yourPlayer, mon1, mon2, mon3, monsterCounter);
+	} while ( yourPlayer.isAlive() && (monsterCounter > 0) );
+
+	if ( yourPlayer.isAlive() ) std::cout << std::endl << "Поздравляем вас с победой!";
+	else std::cout << std::endl << "Игра окончена, вы проиграли";
+
+	return 0;
+}
